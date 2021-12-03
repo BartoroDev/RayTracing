7ac4dda34e72b4e17d179041474584eb368a810a
Bartoro <mateusz.bartosik.private@gmail.com>
2021-12-03 22:08:43 +0100

    Adding Test class and namesoace to project

--- RayTracingProject/src/tests/Test.h
+++ RayTracingProject/src/tests/Test.h
@@ -4,11 +4,11 @@ namespace test {
 	class Test
 	{
 	public:
-		Test();
-		virtual ~Test();
+		Test() {}
+		virtual ~Test() {}
 
-		virtual void OnUpdate(float deltaTime);
-		virtual void OnRender();
-		virtual void OnImGuiRender();
+		virtual void OnUpdate(float deltaTime) {}
+		virtual void OnRender() {}
+		virtual void OnImGuiRender() {}
 	};
 }
